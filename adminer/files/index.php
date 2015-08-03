29afb933d5dd887936c0b57178ff23d7697b915f
Kurt Bendlin <kurt@bendlin.de>
2015-08-03 13:50:48 +0200

    lets see

--- adminer/files/index.php
+++ adminer/files/index.php
@@ -1,2 +1,3 @@
-
-<a href="adminer.php?username=bogus">open adminer for {{ adminer.name }}</a>
+{% for connection in adminer %}
+<li><a href="{{ connection.name }}-adminer.php?username={{ connection.db_user  }}">open adminer {{ connection.name }} (database: {{ connection.db_name }} as user {{ connection.db_user }}</a></li>
+{% endfor %}
