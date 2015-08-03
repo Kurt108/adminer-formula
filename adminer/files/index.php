4f3330219ec8414e71a0772b5c8ef899ad3658ee
Kurt Bendlin <kurt@bendlin.de>
2015-08-03 14:41:10 +0200

    this works as expected

--- adminer/files/index.php
+++ adminer/files/index.php
@@ -1,15 +1,15 @@
 {%- macro print_name(identifier, key) -%}
-{%- if 'name' in key  %}
+{%- if 'name' in key  -%}
 {{ key['name'] }}
-{%- else %}
+{%- else -%}
 {{ identifier }}
-{%- endif %}
+{%- endif -%}
 {%- endmacro -%}
 
 {%- for identifier,keys in connections.iteritems() -%}
   {%- for key in keys -%}
     {% if 'present' in key %}
-<li><a href="{{ print_name(identifier, key) }}-adminer.php?username={{ key['db_user'] }}">open adminer {{ print_name(identifier, key) }} (database: {{ key['db_name'] }} as user {{ key['db_user'] }}</a></li>
+<li><a href="{{ print_name(identifier, key) }}-adminer.php?username={{ key['db_user'] }}">open adminer {{ print_name(identifier, key) }} (database: {{ key['db_name'] }} as user {{ key['db_user'] }})</a></li>
     {% endif %}
   {% endfor %}
 {% endfor %}
