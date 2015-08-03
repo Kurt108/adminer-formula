22e4aa589fbff2ede64d04b91178750de73350f4
Kurt Bendlin <kurt@bendlin.de>
2015-08-03 14:00:41 +0200

    get it round

--- adminer/files/index.php
+++ adminer/files/index.php
@@ -1,3 +1,15 @@
-{% for connection in adminer %}
-<li><a href="{{ connection.name }}-adminer.php?username={{ connection.db_user  }}">open adminer {{ connection.name }} (database: {{ connection.db_name }} as user {{ connection.db_user }}</a></li>
+{%- macro print_name(identifier, key) -%}
+{%- if 'name' in key  %}
+{{ key['name'] }}
+{%- else %}
+{{ identifier }}
+{%- endif %}
+{%- endmacro -%}
+
+{%- for identifier,keys in adminer.iteritems() -%}
+  {%- for key in keys -%}
+    {% if 'present' in key %}
+<li><a href="{{ print_name(identifier, key) }}-adminer.php?username={{ key['db_user'] }}">open adminer {{ print_name(identifier, key) }} (database: {{ key['db_name'] }} as user {{ key['db_user'] }}</a></li>
+    {% endif %}
+  {% endfor %}
 {% endfor %}
